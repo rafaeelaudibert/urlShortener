66c3025a8add7149bd774599ccacb69ed1250fa3
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-13 17:09:55 -0300

    :recycle: Change DB_SECRET to ENDPOINT_SECRET

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-const { MONGO_URI, MONGO_DATABASE, MONGO_COLLECTION, DB_SECRET } = process.env
+const { MONGO_URI, MONGO_DATABASE, MONGO_COLLECTION, ENDPOINT_SECRET } = process.env
 
 module.exports = {
   mongoCollection: MONGO_COLLECTION,
   mongoDatabase: MONGO_DATABASE,
   mongoUri: MONGO_URI,
-  secretKey: DB_SECRET,
+  secretKey: ENDPOINT_SECRET,
 }
