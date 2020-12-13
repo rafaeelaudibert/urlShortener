234276dc1d4ab21a55f1692902387a99b662ff52
RafaAudibert <rafaeelaudibert@gmail.com>
2020-12-13 15:57:42 -0300

    :recycle: Change DB_SECRET env name

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-const { MONGO_URI, MONGO_DATABASE, MONGO_COLLECTION, SECRET } = process.env
+const { MONGO_URI, MONGO_DATABASE, MONGO_COLLECTION, DB_SECRET } = process.env
 
 module.exports = {
   mongoCollection: MONGO_COLLECTION,
   mongoDatabase: MONGO_DATABASE,
   mongoUri: MONGO_URI,
-  secretKey: SECRET,
+  secretKey: DB_SECRET,
 }
